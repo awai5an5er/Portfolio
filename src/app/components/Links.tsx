d3f3e423f7591f6add5ce7238c4b6d17b3d3759c
awai5an5er <awaisanser17@gmail.com>
2025-12-19 18:01:34 +0500

    phone number link added

--- src/app/components/Links.tsx
+++ src/app/components/Links.tsx
@@ -1,15 +1,16 @@
-import { Facebook, Instagram, Twitter, MessageCircleMore } from 'lucide-react'
+import { Facebook, Instagram, Twitter, MessageCircleMore,Phone } from 'lucide-react'
 import Link from 'next/link'
 
 function Links() {
     const Icons = [
+        {Logo:Phone, href:"tel:+923150460352"},
+        {Logo:MessageCircleMore, href:"https://wa.me/+923150460352"},
         {Logo:Instagram, href:"https://www.instagram.com/awais_anser_?igsh=MXhzdmhxbzh0M3B3Zw=="},
         {Logo:Facebook, href:"https://www.facebook.com/awais.anser.520"},
         {Logo:Twitter, href:"https://x.com/awais_anser_?s=21"},
-        {Logo:MessageCircleMore, href:"https://wa.me/+923150460352"}
     ]
   return (
-    <section className="flex gap-10 md:gap-20 items-center justify-center glass rounded-3xl p-6 text-center sm:p-8">
+    <section className="flex gap-8 md:gap-15 items-center justify-center glass rounded-3xl p-6 text-center sm:p-8">
          {Icons.map(({ Logo, href }, index) => (
         <Link
           key={index}
