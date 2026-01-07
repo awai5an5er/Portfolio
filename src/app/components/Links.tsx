a084d6cf326b214f62ebb7a7f52519ad883cd19e
awai5an5er <awaisanser17@gmail.com>
2026-01-07 14:40:32 +0500

    links

--- src/app/components/Links.tsx
+++ src/app/components/Links.tsx
@@ -1,14 +1,23 @@
-import { Facebook, Instagram, Twitter, MessageCircleMore,Phone } from 'lucide-react'
-import Link from 'next/link'
+import {
+  Facebook,
+  Instagram,
+  Twitter,
+  MessageCircleMore,
+  Phone,
+} from "lucide-react";
+import Link from "next/link";
 
 function Links() {
-    const Icons = [
-        {Logo:Phone, href:"tel:+923150460352"},
-        {Logo:MessageCircleMore, href:"https://wa.me/+923150460352"},
-        {Logo:Instagram, href:"https://www.instagram.com/awais_anser_?igsh=MXhzdmhxbzh0M3B3Zw=="},
-        {Logo:Facebook, href:"https://www.facebook.com/awais.anser.520"},
-        {Logo:Twitter, href:"https://x.com/awais_anser_?s=21"},
-    ]
+  const Icons = [
+    { Logo: Phone, href: "tel:+923150460352" },
+    { Logo: MessageCircleMore, href: "https://wa.me/+923150460352" },
+    {
+      Logo: Instagram,
+      href: "https://www.instagram.com/awais_anser_?igsh=MXhzdmhxbzh0M3B3Zw==",
+    },
+    { Logo: Facebook, href: "https://www.facebook.com/awais.anser.520" },
+    { Logo: Twitter, href: "https://x.com/awais_anser_?s=21" },
+  ];
   return (
     <section className="flex gap-10 md:gap-20 items-center justify-center glass rounded-3xl p-6 text-center sm:p-8">
       {Icons.map(({ Logo, href }, index) => (
@@ -19,11 +28,11 @@ function Links() {
           rel="noopener noreferrer"
           className="hover:scale-110 hover:-rotate-3 transition-transform duration-300"
         >
-          <Logo className="text-slate-400 h-5 w-5 md:h-10 md:w-10" />
+          <Logo className="text-slate-400 opacity-80 h-5 w-5 md:h-10 md:w-10" />
         </Link>
       ))}
     </section>
-  )
+  );
 }
 
-export default Links
+export default Links;
